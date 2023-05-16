19ca1ebc73c26f99d3e350b2af43f2aa5cfae47e
chris4567890 <starchristian4@hotmail.com>
2023-05-16 11:10:28 +0200

    added some ints.
    added some ints.

--- src/main/java/philosophers_ice/CombatScene.java
+++ src/main/java/philosophers_ice/CombatScene.java
@@ -1,5 +1,20 @@
 package philosophers_ice;
 
+
+import java.util.ArrayList;
+
 public class CombatScene {
-    
+    private ArrayList<Enemy> enemies;
+    private Player currentPlayer;
+    private int maxInitiative;
+
+
+    void starCombat(){
+        
+    }
+
+    void endCombat(){
+
+    }
+
 }
