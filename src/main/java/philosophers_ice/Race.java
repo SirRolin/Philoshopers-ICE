f133d42f2527a2788228f335ccb43295c6d24236
Sir Rolin <nicolaieno@msn.com>
2023-05-17 19:42:49 +0200

    another step for the parser

--- src/main/java/philosophers_ice/Race.java
+++ src/main/java/philosophers_ice/Race.java
@@ -2,14 +2,20 @@ package philosophers_ice;
 
 import ICE.util.HashMapExplorer;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Race {
+public class Race implements Serializable {
     public String name;
     private String imagePath;
 
     public Race(HashMap map){
         name = HashMapExplorer.getString(map,"name");
         imagePath = HashMapExplorer.getString(map, "imagePath");
+    }
+    //// debugging and temporary
+    public Race(String name, String imagePath){
+        this.name = name;
+        this.imagePath = imagePath;
     }
 }
