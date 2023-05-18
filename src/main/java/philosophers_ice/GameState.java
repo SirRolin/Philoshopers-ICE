a083971649682bfcb817ef001b913b705db5a229
Sir Rolin <nicolaieno@msn.com>
2023-05-18 20:48:58 +0200

    Update GameState.java

--- src/main/java/philosophers_ice/GameState.java
+++ src/main/java/philosophers_ice/GameState.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 public class GameState implements Serializable {
 
     public String name = "";
-    transient MapTile[][] maptile = getMinimap(9);
+    transient MapTile[][] maptile;
     int x = 0;
     int y = 0;
     public GameState(String name){
         this.name = name;
+         maptile = getMinimap(9, 5);
         //StateSaver.saveGame(this);
     }
     GameState(){
@@ -23,12 +24,27 @@ public class GameState implements Serializable {
 
     public Player p1;
 
+<<<<<<< Updated upstream
     public MapTile[][] getMinimap(int size){
         MapTile[][] output = new MapTile[size*2-1][size*2-1];
         for(int i = 0; i < size*2-1; ++i){
             for(int j = 0; j < size*2-1; ++j){
                 output[i][j] = StateSaver.loadMap(this, i + x - size*2, j + y - size*2);
+=======
+    public MapTile[][] getMinimap(int size, int mid){
+        if(mid <= 0){
+            mid = 0;
+        } else if(mid > size){
+            mid = size-1;
+        }
+        MapTile[][] output = new MapTile[size][size];
+        for(int i = 0; i < size; ++i){
+            MapTile[] inner = new MapTile[size];
+            for(int j = 0; j < size; ++j){
+                inner[j] = StateSaver.loadMap(this, i + x - (mid - 1), j + y - (mid - 1));
+>>>>>>> Stashed changes
             }
+            output[i] = inner;
         }
 
         return output;
